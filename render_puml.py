62a2aa2e9a0256f92527497cd209156a749dfe4d
elsholz <hendrik.lankers.hl@googlemail.com>
2021-03-01 20:22:57 +0100

    add theme and update code

--- render_puml.py
+++ render_puml.py
@@ -3,8 +3,8 @@ from subprocess import call
 import shutil
 
 for file in os.listdir('src'):
-    if file.endswith('.puml'):
-        call(["java", "-jar", "plantuml.jar", "src/" + file])
+    if file.endswith('.puml') and not 'theme' in file:
+        call(["java", "-jar", "plantuml.jar", "-config", "src/ferrytix_theme.puml", "src/" + file])
         fname = ''.join(file.split('.')[:-1]) + ".png"
         shutil.move("src/" + fname, "img/" + fname)
 
